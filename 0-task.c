c858a801b483c5875f5a187a4e20dded71c95b5a
1duuude8 <younesarab3@gmail.com>
2023-10-20 15:24:44 -0700

    answer for task 0 printf project

--- 0-task.c
+++ 0-task.c
@@ -1,44 +1,87 @@
-#include "main.h"
-#include <stdarg.h>
-#include <unistd.h>
-#include <stddef.h>
-
+#include"main.h"
 /**
- * print_char - Prints a character to the standard output.
- * @c: Character to be printed.
+ *print_char - prints a character to the standard output.
+ * @c:character to be printed
  *
- * Return: Number of characters printed (always 1).
+ * Return: number of character printed
  */
-int print_char(char c)
+static int print_char(char c)
 {
-    write(1, &c, 1);
-    return 1;
+	return (write(1, &c, 1));
 }
 
 /**
- * print_string - Prints a string to the standard output.
- * @str: String to be printed.
+ * print_string - prints a string to standar output.
+ * @str: string to be printed.
+ *
+ * Return: number of characters printed
+ */
+static int print_string(const char *str)
+{
+	int lenght = 0;
+
+	if (str == NULL)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+	while (*str != '\0')
+	{
+	write(1, str, 1);
+	str++;
+	lenght++;
+	}
+	return (lenght);
+}
+
+/**
+ * print_format - Processes and prints format specifiers.
+ *@format: sting containg format specifiers.
+ *@args: Arguments to be fprmatted amd printed.
+ *
+ * Description:
+ * This function processes the format string and optional arguments, handling
+ * %c, %s, and %% for printing %. It prints the
+ * formatted text to the standard output.
  *
  * Return: Number of characters printed (excluding null byte).
  */
-int print_string(char *str)
+int print_format(const char *format, va_list args)
 {
-    int length = 0;
-    if (str == NULL)
-    {
-        write(1, "(null)", 6);
-        length = 6;
-    }
-    else
-    {
-        while (*str)
-        {
-            write(1, str, 1);
-            str++;
-            length++;
-        }
-    }
-    return length;
+	int to_print = 0;
+	char cc;
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+		to_print += print_char(*format);
+		}
+	else
+	{
+		format++;
+		if (*format == '\0')
+			break;
+
+		if (*format == 'c')
+		{
+			cc = va_arg(args, int);
+			to_print += print_char(cc);
+		}
+		else if (*format == 's')
+		{
+			char *strg = va_arg(args, char *);
+
+			to_print += print_string(strg);
+		}
+		else if (*format == '%')
+		{
+			to_print += print_char('%');
+		}
+	}
+	format++;
+	}
+	return (to_print);
 }
 
 /**
@@ -54,47 +97,17 @@ int print_string(char *str)
  */
 int _printf(const char *format, ...)
 {
-    int to_print = 0;
-    va_list args;
+	int to_print = 0;
+	va_list args;
 
-    if (format == NULL)
-        return -1;
+	if (format == NULL)
+		return (-1);
 
-    va_start(args, format);
-
-    while (*format)
-    {
-        if (*format != '%')
-        {
-            to_print += print_char(*format);
-        }
-        else
-        {
-            format++;
-            if (*format == '\0')
-                break;
-            if (*format == '%')
-            {
-                to_print += print_char('%');
-            }
-            else if (*format == 'c')
-            {
-                char cc = va_arg(args, int);
-                to_print += print_char(cc);
-            }
-            else if (*format == 's')
-            {
-                char *strg = va_arg(args, char *);
-                to_print += print_string(strg);
-            }
-            else
-            {
-                to_print += print_string("(null)");
-            }
-        }
-        format++;
-    }
-
-    va_end(args);
-    return to_print;
+	va_start(args, format);
+	to_print = print_format(format, args);
+	va_end(args);
+	return (to_print);
 }
+
+
+
--- main.h
+++ main.h
@@ -2,7 +2,9 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <unistd.h>
 
+/* function prototypes */
 int _printf(const char *format, ...);
 
-#endif /* MAIN_H */
+#endif
